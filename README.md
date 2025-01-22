e548c6027d4f0dfa923deeba15eb6b2b6570dfc0
vickyqb <vicky.kumar@quadbtech.com>
2025-01-22 12:16:36 +0000

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `internet_identity`
+# `Integration of internet_identity and NFID in ICP`
 
 Welcome to your new `internet_identity` project and to the Internet Computer development community. By default, creating a new project adds this README and some template files to your project directory. You can edit these template files to customize your project and to include your own code to speed up the development cycle.
 
--- src/internet_identity_frontend/src/App.jsx
+++ src/internet_identity_frontend/src/App.jsx
@@ -1,17 +1,52 @@
-import { useState } from 'react';
-import { internet_identity_backend } from 'declarations/internet_identity_backend';
+import { useState ,useEffect} from 'react';
+import { internet_identity_backend } from '../../declarations/internet_identity_backend';
+import {AuthClient} from '@dfinity/auth-client';
+import { IdentityKitProvider, IdentityKitTheme } from "@nfid/identitykit/react"
+import { NFIDW, IdentityKitAuthType ,Plug, InternetIdentity, Stoic} from "@nfid/identitykit"
+import "@nfid/identitykit/react/styles.css"
+import { ConnectWallet } from "@nfid/identitykit/react"
+
 
 function App() {
   const [greeting, setGreeting] = useState('');
+  const [authClient, setAuthClient] = useState(null);
+  const [authenticated, setAuthenticated] = useState(false);
 
-  function handleSubmit(event) {
+  const handleSubmit = (event)=> {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    internet_identity_backend.greet(name).then((greeting) => {
+    ii_icp_backend.greet(name).then((greeting) => {
       setGreeting(greeting);
     });
     return false;
   }
+  const handleLogin = async ()=> {
+    await authClient.login({
+      identityProvider: process.env.DFX_NETWORK === "ic"
+                            ? "https://identity.ic0.app/"
+                            : "http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943",
+      onError: (error) => {
+        setAuthenticated(false);
+        console.error(error);
+      },
+      onSuccess: () => {
+        setAuthenticated(true);
+        console.log('Logged in');
+      },
+    });
+  }
+  const handleLogout = async ()=> {
+    await authClient?.logout();
+    setAuthenticated(false);
+    console.log('Logged out');
+  }
+
+  useEffect(() => {
+      (async () => {
+          const authClient = await AuthClient.create();
+          setAuthClient(authClient);
+      })();
+  }, []);
 
   return (
     <main>
@@ -19,10 +54,24 @@ function App() {
       <br />
       <br />
       <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+        <label htmlFor="name" className='form-label'>Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" class = "form-control "  />
+        <button type="submit" className="btn btn-primary">Greet!</button>
       </form>
+      <div className='container d-flex align-items-center justify-content-around'>
+        <div className='login-button'>
+          {authenticated ? <button onClick={handleLogout} className="btn btn-danger">Logout</button>:<button onClick={handleLogin} className="btn btn-primary">Login with ii</button>}
+        </div>
+        <div className='wallet-button'> 
+          <IdentityKitProvider 
+            signers={[NFIDW,Plug, InternetIdentity, Stoic]}
+            theme={IdentityKitTheme.SYSTEM} // LIGHT, DARK, SYSTEM (by default)
+            authType={IdentityKitAuthType.DELEGATION} // ACCOUNTS, DELEGATION (by default)
+            >
+            <ConnectWallet/>
+          </IdentityKitProvider>
+        </div>
+      </div>
       <section id="greeting">{greeting}</section>
     </main>
   );
